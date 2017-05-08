a1cc41948aa9297d0c955505bb6a74662e3238b7
Patrick Hannak <patrick.hannak@alumni.fh-aachen.de>
2017-05-08 13:49:33 +0200

    turning publisher added

---
+++ maskor_ev3/src/turn_left_for_x_secs.cpp
@@ -0,0 +1,26 @@
+#include "ros.h"
+#include "ros/time.h"
+#include "maskor_ev3/turn_left_for_x_secs.h"
+#include "geometry_msgs/Twist.h"
+
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "turn_left_pub");
+  ros::NodeHandle n;
+
+  ros::Publisher pub = n.advertise<geometry_msgs::Twist>("turn_left", 1000);
+  ros::Rate loop_rate(10);
+  
+  ROS_INFO("Ready to turn!");
+
+  while(ros::ok())
+    {
+      geometry_msgs::Twist msg;
+      msg.linear.x=10;
+      msg.angular.z=10;
+      pub.publish(msg);
+      rate.sleep();
+    }
+  return 0;
+}
