9deb8e183540dcd172a57d92e3d834e8ae3cfeb4
Dennis Miltz <dennis.miltz@4alumni.fh-aachen.de>
2017-05-16 15:13:05 +0200

    change line_follower_node

--- maskor_ev3_gazebo/src/maskor_ev3_line_follower_node.cpp
+++ maskor_ev3_gazebo/src/maskor_ev3_line_follower_node.cpp
@@ -10,7 +10,11 @@ public:
 private:
   void lineFollowerCallback(const std_msgs::Int8::ConstPtr& msg);
 
-  
+  bool lastblackr = false;
+  bool lastblackl = false;
+  bool drive[5] {false};
+  bool tracknotOk = true;
+  int count = 0;
 
   ros::Subscriber sub;
   ros::Publisher vel_pub_;
@@ -28,29 +32,100 @@ LineFollower::LineFollower(){
 
 void LineFollower::lineFollowerCallback(const std_msgs::Int8::ConstPtr& msg)
 {
-  ROS_INFO("I heard: [%i]", msg->data);
+  //ROS_INFO("I heard: [%i]", msg->data);
   geometry_msgs::Twist twist;
-  bool trackOk = true;
+ //finde linke Seite
+ if(lastblackl == false && lastblackr == false){
+   drive[3] = true;
+   if(msg->data == 0){
+     lastblackr = true;
+   }
+ }
+ else{
+   if(msg->data == 1){
+     drive[0] =true;
+     drive[1] =false;
+     drive[2] =false;
+     drive[3] =false;
+     count = 0;
+   }
+   else{
+     if(lastblackl){
+      drive[0] = false;
+      drive[1] = false;
+      drive[2] = false;
+      drive[3] = true;
 
-  if(msg->data != 1){
-    trackOk = false;
+
+      if(msg->data == 1){
+        drive[0] =true;
+        drive[1] =false;
+        drive[2] =false;
+        drive[3] =false;
+        lastblackr = false;
+        lastblackl = true;
+      }
+     }
+     else if(lastblackr){
+       drive[0] = false;
+       drive[1] = false;
+       drive[2] = true;
+       drive[3] = false;
+
+       if(msg->data == 1){
+         drive[0] =true;
+         drive[1] =false;
+         drive[2] =false;
+         drive[3] =false;
+         lastblackr = true;
+         lastblackl = false;
+       }
+     }
+   }
+ }
+
+
+
+
+
+
+  //VORWÄRTS
+  if(drive[0]){
+    std::cout << "vörwärts" << '\n';
+    twist.linear.x = 2;
+    twist.angular.z = 0;
+    //vel_pub_.publish(twist);
   }
-
-
-
-    if(trackOk){
-      twist.linear.x = 2;
-      twist.angular.z = 0;
-      vel_pub_.publish(twist);
-    }
-    else{
-      twist.linear.x = 0;
-      twist.angular.z = 2;
-      vel_pub_.publish(twist);
-
-
-    }
-
+  //RÜCKWÄRTS
+  else if(drive[1]){
+    std::cout << "rückwärts" << '\n';
+    //lastblackr = false;
+    twist.linear.x = -2;
+    twist.angular.z = 0;
+    //vel_pub_.publish(twist);
+  }
+  //RECHTS
+  else if(drive[2]){
+    std::cout << "rechts" << '\n';
+    //lastblackr = false;
+    twist.linear.x = 0;
+    twist.angular.z = -20;
+    //vel_pub_.publish(twist);
+  }
+  //LINKS
+  else if(drive[3]){
+    std::cout << "links" << '\n';
+    twist.linear.x = 0;
+    twist.angular.z = 20;
+    //vel_pub_.publish(twist);
+  }
+  //STOPP
+  else if(drive[4]){
+    std::cout << "stopp" << '\n';
+    twist.linear.x = 0;
+    twist.angular.z = 0;
+    //vel_pub_.publish(twist);
+  }
 
 }
 
