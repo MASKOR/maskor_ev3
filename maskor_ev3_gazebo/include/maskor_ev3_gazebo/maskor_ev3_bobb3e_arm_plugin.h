334953fe75140859f167dd687f1a559319472e88
Marcel Stüttgen <stuettgen@fh-aachen.de>
2017-06-06 16:43:37 +0200

    started refactoring of arm plugin

--- maskor_ev3_gazebo/include/maskor_ev3_gazebo/maskor_ev3_bobb3e_arm_plugin.h
+++ maskor_ev3_gazebo/include/maskor_ev3_gazebo/maskor_ev3_bobb3e_arm_plugin.h
@@ -11,11 +11,8 @@
 #include <tf/transform_broadcaster.h>
 #include <tf/transform_listener.h>
 #include <geometry_msgs/Twist.h>
-#include <nav_msgs/Odometry.h>
-#include <nav_msgs/OccupancyGrid.h>
 #include <sensor_msgs/JointState.h>
 #include <gazebo_plugins/gazebo_ros_utils.h>
-
 
 // Custom Callback Queue
 #include <ros/callback_queue.h>
@@ -27,25 +24,30 @@
 
 namespace gazebo {
 
+  enum{
+    RIGHT_FORK_ARM=0,
+    LEFT_FORK_ARM=1,
+    FORK_LIFT=2,
+    NUM_JOINTS=3
+  };
+
   class Joint;
   class Entity;
 
   class MaskorEv3ArmPlugin : public ModelPlugin {
 
   public:
-  MaskorEv3ArmPlugin();
+    MaskorEv3ArmPlugin();
     ~MaskorEv3ArmPlugin();
     void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf);
 
   protected:
     virtual void UpdateChild();
     virtual void FiniChild();
-    void publishJointStates();
 
   private:
-    void publishOdometry(double step_time);
     void getForkVelocities();
-
+    void publishJointStates();
 
     physics::WorldPtr world;
     physics::ModelPtr parent;
@@ -54,32 +56,25 @@ namespace gazebo {
     std::string right_fork_joint_name_;
     std::string left_fork_joint_name_;
     std::string fork_lift_joint_name_;
+    std::string command_topic_;
 
     GazeboRosPtr gazebo_ros_;
 
     double fork_torque;
     double fork_speed_;
 
-    physics::JointPtr joints[3];
+    physics::JointPtr joints[NUM_JOINTS];
 
     //ROS Stuff
     ros::NodeHandle* rosnode_;
     ros::Subscriber cmd_vel_subscriber_;
-    ros::Publisher odometry_publisher_;
-    tf::TransformBroadcaster *transform_broadcaster_;
-    nav_msgs::Odometry odom_;
     std::string tf_prefix_;
-    bool broadcast_tf_;
     sensor_msgs::JointState joint_state_;
     ros::Publisher joint_state_publisher_;
 
     boost::mutex lock;
 
-    std::string turtle_name;
     std::string robot_namespace_;
-    std::string command_topic_;
-    std::string robot_base_frame_;
-    std::string odometry_frame_;
 
     // Custom Callback Queue
     ros::CallbackQueue queue_;
@@ -92,17 +87,12 @@ namespace gazebo {
     double z_;
     double rot_;
     bool alive_;
+    bool publishJointStates_;
 
     // Update Rate
     double update_rate_;
     double update_period_;
     common::Time last_update_time_;
-
-    double covariance_x_;
-    double covariance_y_;
-    double covariance_yaw_;
-
-    bool publishJointStates_;
 
   };
 }
